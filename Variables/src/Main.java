e4babff9afa367a81286e8251d2b37125e075191
Oğuzhan <100987857+oguzhanckmk@users.noreply.github.com>
2022-10-06 23:00:44 +0300

    Add files via upload

---
+++ Variables/src/Main.java
@@ -0,0 +1,15 @@
+//Variables - degiskenler
+public class Main {
+    public static void main(String[] args) {
+    // Java case sensitive (buyuk kucuk duyarlı) bir dildir.
+        int ogrenciSayisi = 10;
+        String mesaj = "Öğrenci sayısı";
+        System.out.println(" mesaj :"+ogrenciSayisi);
+        System.out.println(" mesaj :"+ogrenciSayisi);
+
+
+// degiskenleri isimlendirirken iki kelimeden olusuyorsa ikinci kelimenin ilk harfi genellikle buyuk yazilir. Buna da camel casing adı verilir.
+// reusability - tekrar tekrar kullanılabilirlik;
+
+    }
+}
